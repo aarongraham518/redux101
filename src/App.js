22056395c2334c0f3857bf0d3825721dd040dd36
Aaron Graham <aarongraham518@gmail.com>
2020-09-07 00:59:35 -0400

    Added store, reducer, actions and dispatch to the project. Reducers are in different files along with actions. Provider is implemented as well

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
-  return (
+  //access to global state
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  
+  //Adding dispatch to execute actions to our reducer
+  const dispatch = useDispatch();
+  const dispatch1 = useDispatch();
+
+  return (    
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch1(decrement())}>-</button>
+
+      {isLogged ? <h3>Valuable information I should not see</h3> : ''}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,49 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
+import allReducers from './reducers';
+import {Provider} from 'react-redux';
 
 
-//STORE, which is a globalized data/state.
-
-//ACTION, (INCREMENT)describes what we want to do
-//an Action is a function that returns an Object
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-//REDUCER, describes how our actions transform the state into the next state
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case "INCREMENT":
-        return state + 1;
-    case "DECREMENT":
-        return state - 1;
-
-  }
-};
-
-//Add counter to the store
-let store = createStore(counter);
-
-//Display in console
-store.subscribe(() => console.log(store.getState()));
-
-/*DISPATCH, execute this Action to the reducer, the Reducer will check
-what to do and the Store gets updated :|*/
-store.dispatch(increment);
-store.dispatch(decrement);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
