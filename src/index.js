e45590d5e68cd599925fb06c7a2d3fbef5d7a36c
Aaron Graham <aarongraham518@gmail.com>
2020-09-06 20:12:53 -0400

    setup store, actions, reducer and dispatch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+
+//STORE, which is a globalized data/state.
+
+//ACTION, (INCREMENT)describes what we want to do
+//an Action is a function that returns an Object
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//REDUCER, describes how our actions transform the state into the next state
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case "INCREMENT":
+        return state + 1;
+    case "DECREMENT":
+        return state - 1;
+
+  }
+};
+
+//Add counter to the store
+let store = createStore(counter);
+
+//Display in console
+store.subscribe(() => console.log(store.getState()));
+
+/*DISPATCH, execute this Action to the reducer, the Reducer will check
+what to do and the Store gets updated :|*/
+store.dispatch(increment);
+store.dispatch(decrement);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +51,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
